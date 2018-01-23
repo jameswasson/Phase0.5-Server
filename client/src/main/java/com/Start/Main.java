f50dc2d16e5818a05cee643f2399a350279230c2
James Wasson <jameswasson@users.noreply.github.com>
2018-01-23 14:41:52 -0700

    This is the second edition

--- client/src/main/java/com/Start/Main.java
+++ client/src/main/java/com/Start/Main.java
@@ -3,7 +3,6 @@ package com.Start;
 public class Main {
     public static void main(String args[]){
         testEverythingOnce();
-        testEverythingOnce();
     }
     private static void testEverythingOnce(){
         int parse1 = 3;
